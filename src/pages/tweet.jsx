6796ac2b0f1b22636586168b221fe80f90681d8a
Edonak <edouanakasila69@gmail.com>
2023-11-24 11:13:22 +0000

    refactoring code

--- src/pages/tweet.jsx
+++ src/pages/tweet.jsx
@@ -3,48 +3,48 @@ import Image from "./../images/tweet-profile-photo.png";
 import ImageAvatar from "./../images/tweet-image.png";
 import LogoTweet from "./../images/Tweet Photo.png";
 import LogoAvatar from "./../images/Tweet-Profile-Photo (1).png";
+import ImageVerified from "./../images/Verified.png";
 import ImagesTweet from "./../images/Frame 8.png";
+
+function Verfied() {
+  <img src={ImageVerified} alt="" srcset="" />
+}
 
 function LogoTweeter() {
   return <img src={LogoTweet} alt="" srcset="" />;
 }
 
 function TweetAction() {
-  return (
-    <div className="tweet-action">
-      <div className="tweet-actions">
-        <img src={ImagesTweet} alt="" srcset="" />
-      </div>
-    </div>
-  );
+  return <img src={ImagesTweet} alt="" srcset="" />;
 }
 
 function TweetDetail({ value }) {
   return <span className="tweet-title-details">{value}</span>;
 }
-
-
 function Tweet() {
   return (
     <div className="tweets">
       <div className="tweet">
-        <div className="tweet-avatar">
-          <img src={Image} alt="img" srcset="" />
+        <div className="avatar">
+        <img src={Image} alt="img" srcset="" />
         </div>
-        <div className="">
-          <p className="tweet-title">
-            {" "}
-            CNN
-            <TweetDetail value="@CNN . 7m" />
-          </p>
-          <p className="tweet-text">
-            President Joe Biden touted a new agreement reached with the European
-            Union to ease Trump-era tariffs on aluminum and steel as a "major
-            breakthrough" that would serve to both strengthen the US steel
-            industry and combat the global climate crisis.
-          </p>
-
-          <TweetAction />
+        <div className="tweet-content">
+          <div className="tweet-body">
+            <p className="tweet-title">
+              CNN <Verfied /> <TweetDetail value="@CNN . 7m" />
+            </p>
+            <p className="tweet-text">
+              President Joe Biden touted a new agreement reached with the
+              European Union to ease Trump-era tariffs on aluminum and steel as
+              a "major breakthrough" that would serve to both strengthen the US
+              steel industry and combat the global climate crisis.
+            </p>
+            <div className="tweet-action">
+              <div className="tweet-actions">
+                <TweetAction />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
@@ -54,9 +54,7 @@ function Tweet() {
         </div>
         <div className="tweet-content">
           <div className="tweet-body">
-            <p className="tweet-title">
-              The New York Times <TweetDetail value="@nytimes .2h" />
-            </p>
+            <p className="tweet-title">The New York Times <Verfied /> <TweetDetail value="@nytimes . 2h" /></p>
             <p className="tweet-text">
               Gardening boomed during the pandemic. Six Black writers share how
               it has helped them re-establish, and reimagine, a connection to
@@ -66,16 +64,16 @@ function Tweet() {
               <img src={ImageAvatar} alt="image" srcset="" />
             </div>
           </div>
-          <TweetAction />
+          <div className="tweet-actions">
+            <TweetAction />
+          </div>
         </div>
       </div>
       <div className="tweet">
         <LogoTweeter />
 
         <div className="tweet-content">
-          <p className="tweet-title-detail">
-            Twitter <TweetDetail value="@Twitter . Oct 29" />{" "}
-          </p>
+          <p className="tweet-title-detail">Twitter <TweetDetail value="@Twitter . Oct 29"/> </p>
           <p className="tweet-text">BIG NEWS lol jk still Twitter.</p>
           <div className="tweet-action">
             <div className="tweet-actions">
@@ -87,21 +85,25 @@ function Tweet() {
       <div className="tweet">
         <LogoTweeter />
         <div className="tweet-content">
-          <p className="tweet-title-detail">
-            Twitter <TweetDetail value="@Twitter . Oct 4" />{" "}
-          </p>
+          <p className="tweet-title-detail">Twitter <TweetDetail value="@Twitter . Oct 4" /></p>
           <p className="tweet-text">hello literally everyone</p>
-          <TweetAction />
+          <div className="tweet-action">
+            <div className="tweet-actions">
+              <TweetAction />
+            </div>
+          </div>
         </div>
       </div>
       <div className="tweet">
         <LogoTweeter />
         <div className="tweet-content">
-          <p className="tweet-title-detail">
-            Twitter <TweetDetail value="@Twitter . Oct 4" />{" "}
-          </p>
+          <p className="tweet-title-detail">Twitter <TweetDetail value="@Twitter . Oct 4" /> </p>
           <p className="tweet-text">hello literally everyone</p>
-          <TweetAction />
+          <div className="tweet-action">
+            <div className="tweet-actions">
+              <TweetAction />
+            </div>
+          </div>
         </div>
       </div>
     </div>
