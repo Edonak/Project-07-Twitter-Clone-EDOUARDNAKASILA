43535f47e62423f900a739415d3985d6c93dbccd
Edonak <edouanakasila69@gmail.com>
2024-01-11 09:41:41 +0000

    added tailwind css

--- src/components/tweets.jsx
+++ src/components/tweets.jsx
@@ -1,41 +1,46 @@
-import React from 'react';
-import TweetActions from './tweetActions';
+import React from "react";
+import TweetActions from "./tweetActions";
 
-export default function Tweets({id,  avatar, author, certificate, username, date, content, image, like, comment, retweet}) {
+export default function Tweets({
+  id,
+  avatar,
+  author,
+  certificate,
+  username,
+  date,
+  content,
+  image,
+  like,
+  comment,
+  retweet,
+}) {
   return (
-    <div className='tweet'key={id}>
-    <div className="tweet-avatar">
+    <div className='tweet' key={id}>
+      <div className="tweet-avatar">
         <img src={avatar} alt="" />
-    </div>
-    <div className="tweet-content">
+      </div>
+      <div className="tweet-content flex">
         <div className="tweet-body">
-            <div className="tweet-title">
-                <span className='tweet-title-author'>{author}</span>
-                <img src={certificate} alt="image certificate" className="tweet-certificate" />
-                <span className="tweet-title-details">@{username}</span>
-                <span className="tweet-title-details">.</span>
-                <span className="tweet-title-details">{date}</span>
+          <div className="tweet-title">
+            <span className="tweet-title-author">{author}</span>
+            <img
+              src={certificate}
+              alt="image certificate"
+              className="tweet-certificate"
+            />
+            <span className="tweet-title-details">@{username}</span>
+            <span className="tweet-title-details">.</span>
+            <span className="tweet-title-details">{date}</span>
+          </div>
+          <div className="tweet-text">{content}</div>
+          {image ? (
+            <div className="tweet-image">
+              <img src={image} alt="" />
             </div>
-            <div className="tweet-text">{content}</div>
-            { image ?
-                <div className="tweet-image">
-                    <img src={image} alt="" />
-                </div> :
-                null 
-            }
+          ) : null}
         </div>
-        <TweetActions
-         like={like}
-         comment={comment}
-         retweet={retweet}
-         />
+        <TweetActions like={like} comment={comment} retweet={retweet} />
+      </div>
     </div>
-</div>
-  )
+  );
 }
-
-
-
-
-
-
