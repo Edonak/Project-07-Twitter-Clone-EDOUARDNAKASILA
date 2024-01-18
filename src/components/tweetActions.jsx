d77cd39291f80e1e7fa4e917d9132a127ec46748
Edonak <edouanakasila69@gmail.com>
2024-01-18 11:20:26 +0000

    tweet action modified

--- src/components/tweetActions.jsx
+++ src/components/tweetActions.jsx
@@ -1,46 +1,61 @@
-import React, {useState, createContext, useContext} from 'react';
-import CommentIcon from './../images/commentIcon.svg';
-import RetweetIcon from './../images/retweetIcon .svg';
-import LikeIcon from './../images/like.svg';
-import ShareIcon from './../images/shareIcon.svg';
+import React, { useState, createContext, useContext } from "react";
+import CommentIcon from "./../images/commentIcon.svg";
+import RetweetIcon from "./../images/retweetIcon .svg";
+import LikeIcon from "./../images/like.svg";
+import ShareIcon from "./../images/shareIcon.svg";
 
-export default function TweetActions({comment, retweet, like}) {
+export default function TweetActions({ comment, retweet, like }) {
+  const [color1, setColor1] = useState('');
+  const [color2, setColor2] = useState('');
+  const [color3, setColor3] = useState('');
 
-    const [color1, setColor1] = useState("");
-    const [color2, setColor2] = useState("");
-    const [color3, setColor3] = useState("");
+  const handleHover1 = () => {
+    setColor1('green');
+  };
 
-  
-    const handleClick1 = () => {
-      setColor1("green");
-    };
-  
-    const handleClick2 = () => {
-      setColor2("blue");
-    };
-  
-    const handleClick3 = () => {
-      setColor3("red");
-    };
-  
+  const handleHover2 = () => {
+    setColor2('blue');
+  };
+
+  const handleHover3 = () => {
+    setColor3('red');
+  };
 
   return (
     <div className="tweet-actions">
-    <div className="tweet-action" title="Comments" onClick={handleClick1} style={{color: color1 }}>
-        <img src={CommentIcon} alt="" style={{color: color1 }}/>
+      <div
+        className="tweet-action"
+        title="Comments" 
+        onMouseEnter={handleHover1}
+        onMouseLeave={() => setColor1('')}
+        style={{ color: color1 }}
+      >
+        <img src={CommentIcon} alt="" style={{ color: color1 }}  />
         {comment}
-    </div>
-    <div className="tweet-action" title="Retweets"  onClick={handleClick2} style={{ color: color2 }}>
-        <img src={RetweetIcon} alt="" />
+      </div>
+      <div
+        className="tweet-action"
+        title="Retweets"
+        onMouseEnter={handleHover2}
+        onMouseLeave={() => setColor2('')}
+        style={{ color: color2 }}
+      >
+        <img src={RetweetIcon} alt="retweet image" style={{ color: color2 }} />
         {retweet}
-    </div>
-    <div className="tweet-action" title="Likes"  onClick={handleClick3} style={{ color: color3 }}>
-        <img src={LikeIcon} alt="" />
+      </div>
+      <div
+        className="tweet-action"
+        title="Likes"
+        onMouseEnter={handleHover3}
+        onMouseLeave={() => setColor3('')}
+        style={{ color: color3 }}
+      >
+        <img src={LikeIcon} alt="like image" style={{ color: color3 }} />
         {like}
+      </div>
+      <div className="tweet-action" title="Share">
+        <img src={ShareIcon} alt="share image" />
+      </div>
     </div>
-    <div className="tweet-action" title="Share">
-        <img src={ShareIcon} alt="" />
-    </div>
-</div>
-  )
+  );
 }
